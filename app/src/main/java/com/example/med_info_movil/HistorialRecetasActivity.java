d1d07defe699b67fd94cd57ddadb7651f7254c91
JuanCarlosRF79 <juan.ruvalcaba@ia-society.com>
2023-10-28 19:01:45 -0600

    Creación de Activities principales

---
+++ app/src/main/java/com/example/med_info_movil/HistorialRecetasActivity.java
@@ -0,0 +1,14 @@
+package com.example.med_info_movil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistorialRecetasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial_recetas);
+    }
+}
---
+++ app/src/main/java/com/example/med_info_movil/RegitroActivity.java
@@ -0,0 +1,14 @@
+package com.example.med_info_movil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegitroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regitro);
+    }
+}
