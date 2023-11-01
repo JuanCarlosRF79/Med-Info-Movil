c113c45ac7309f46203539000b11ff7a9cfb7ef7
JuanCarlosRF79 <juan.ruvalcaba@ia-society.com>
2023-10-31 20:48:28 -0600

    Implementar menu de navegación por botones

--- app/src/main/java/com/example/med_info_movil/MainActivity.kt
+++ app/src/main/java/com/example/med_info_movil/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.med_info_movil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public fun irMenu(view:View){
+        var intent:Intent = Intent(this,MenuPrincipal::class.java)
+        startActivity(intent)
+        finish()
     }
 }
