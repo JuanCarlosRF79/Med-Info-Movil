3a1c1c22621b9b149a4cc70a4541d08f73a7ae36
JuanCarlosRF79 <juan.ruvalcaba@ia-society.com>
2023-11-12 16:29:35 -0600

    Tomar foto de medicamento y mostrarla en un dialog

--- app/src/main/java/com/example/med_info_movil/DetalleRecetaActivity.java
+++ app/src/main/java/com/example/med_info_movil/DetalleRecetaActivity.java
@@ -7,23 +7,32 @@ import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.med_info_movil.clases.NotificacionAlarma;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.ByteArrayOutputStream;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Calendar;
 
 
@@ -33,11 +42,14 @@ public class DetalleRecetaActivity extends AppCompatActivity {
     private Button btnHora;
     private CheckBox cbTodosDias,cbLunes,cbMartes,cbMiercoles,cbJueves,cbViernes,cbSabado,cbDomingo;
     private LinearLayout elegirDias;
-    private int horaAlarma,minutoAlarma;
-    private int diaSemana;
+    private ImageView imgnMedicamento;
+    private int horaAlarma,minutoAlarma, horaRepetir,diaSemana;
+    private String imgEncoded;
     private ArrayList<Integer> arrayIDAlarma = new ArrayList<Integer>();
-
     private String[] opcionesSpn = new String[]{};
+
+    private Bitmap bitmap;
+    private byte[] bArray;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -55,6 +67,7 @@ public class DetalleRecetaActivity extends AppCompatActivity {
         cbSabado = findViewById(R.id.cbSabado);
         cbDomingo = findViewById(R.id.cbDomingo);
         elegirDias = findViewById(R.id.elegirDias);
+        imgnMedicamento = findViewById(R.id.imgMedicamento);
 
         cbTodosDias.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,35 +83,20 @@ public class DetalleRecetaActivity extends AppCompatActivity {
         //Buscar si están los IDS de las alarmas y obtener el arreglo
         SharedPreferences preferences = getSharedPreferences("medinfo.dat",MODE_PRIVATE);
         if(preferences.contains("idAlarmas")) arrayIDAlarma=getArrayList("idAlarmas");
+
+        //Llenar imagen en caso de tenerla
+        if(bitmap!=null){ imgnMedicamento.setImageBitmap(bitmap); }
+
+        //Mostrar imagen de médicamento más grande
+        imgnMedicamento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String titulo = "Imagen de prueba";
+                mostrarImagen(view,titulo,bitmap);
+            }
+        });
     }
 
-    public void desactivarCheck(View view){
-        cbLunes.setClickable(false);
-        cbMartes.setClickable(false);
-        cbMiercoles.setClickable(false);
-        cbJueves.setClickable(false);
-        cbViernes.setClickable(false);
-        cbSabado.setClickable(false);
-        cbDomingo.setClickable(false);
-
-        cbLunes.setChecked(false);
-        cbMartes.setChecked(false);
-        cbMiercoles.setChecked(false);
-        cbJueves.setChecked(false);
-        cbViernes.setChecked(false);
-        cbSabado.setChecked(false);
-        cbDomingo.setChecked(false);
-    }
-
-    public void activarCheck(View view){
-        cbLunes.setClickable(true);
-        cbMartes.setClickable(true);
-        cbMiercoles.setClickable(true);
-        cbJueves.setClickable(true);
-        cbViernes.setClickable(true);
-        cbSabado.setClickable(true);
-        cbDomingo.setClickable(true);
-    }
 
     public void setHorarioAlarma(View view) {
         //Instancia para calendario
@@ -120,6 +118,8 @@ public class DetalleRecetaActivity extends AppCompatActivity {
         }
 
         public void programarAlarma(View view) {
+
+        horaRepetir = 1;
             //A la hora de programar, revisar arreglo de días
             // si tiene distintos días crear la misma alarma pero para cada día
             // ArrayList<Integer> alarmDays = new ArrayList<Integer>();
@@ -150,18 +150,78 @@ public class DetalleRecetaActivity extends AppCompatActivity {
 
             AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
             //Repetrir cada semana 7 * 24 * 60 * 60 * 1000
-            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calender.getTimeInMillis(), 1000 * 60 , pendingIntent);
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calender.getTimeInMillis(), 1000 * 60 * 60 * horaRepetir , pendingIntent);
 
             Toast.makeText(this, "Alarma programada"+horaAlarma+" "+minutoAlarma, Toast.LENGTH_SHORT).show();
         }
 
-        public void ocultarDias(){
-            ViewGroup.LayoutParams params = null;
-            elegirDias.setVisibility(View.INVISIBLE);
-            params = elegirDias.getLayoutParams();
-            params.width = 0;
-            params.height = 0;
+        //Funciones para manejar imagenes
+        public void mostrarImagen(View view,String titulo,Bitmap bMap){
+            //Crear AlertDialog en la vista
+            AlertDialog.Builder builder = new AlertDialog.Builder(DetalleRecetaActivity.this);
+
+            //Obtener el content
+            ViewGroup viewGroup = findViewById(android.R.id.content);
+
+            //Llenar un objeto con la vista personalizada del Alert
+            View dialogView = LayoutInflater.from(view.getContext()).inflate(R.layout.layout_mostrar_imagen, viewGroup, false);
+
+            //Obtener los elementos dentro de la lista personalizada
+            @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+            TextView txtMensaje = dialogView.findViewById(R.id.txtMensaje);
+            @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+            ImageView imgMuestra = dialogView.findViewById(R.id.imgMuestra);
+
+            //Llenar con información los textos
+            txtMensaje.setText(titulo);
+            imgMuestra.setImageBitmap(bMap);
+
+            //Construir la vista
+            builder.setView(dialogView);
+            AlertDialog alertDialog = builder.create();
+
+            //Mostrar la vista
+            alertDialog.show();
         }
+
+    public void tomarFoto(View view){
+        abrirCamara();
+    }//tomarFoto
+
+    private void abrirCamara(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(intent,1);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(this, "Tomar foto", Toast.LENGTH_SHORT).show();
+
+        if( requestCode == 1 && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            bitmap = (Bitmap) extras.get("data");
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+            byte[] byteArray = byteArrayOutputStream.toByteArray();
+            imgEncoded = Base64.getEncoder().encodeToString(byteArray);
+
+            imgnMedicamento.setImageBitmap(bitmap);
+        }
+    }
+
+    //   Funciones de apoyo ========================================================================
+
+    public void ocultarDias(){
+        ViewGroup.LayoutParams params = null;
+        elegirDias.setVisibility(View.INVISIBLE);
+        params = elegirDias.getLayoutParams();
+        params.width = 0;
+        params.height = 0;
+    }
 
     public void saveArrayList(ArrayList<Integer> list, String key){
         SharedPreferences prefs = getSharedPreferences("medinfo.dat",MODE_PRIVATE);
@@ -179,6 +239,34 @@ public class DetalleRecetaActivity extends AppCompatActivity {
         String json = prefs.getString(key, null);
         Type type = new TypeToken<ArrayList<Integer>>() {}.getType();
         return gson.fromJson(json, type);
+    }
+
+    public void desactivarCheck(View view){
+        cbLunes.setClickable(false);
+        cbMartes.setClickable(false);
+        cbMiercoles.setClickable(false);
+        cbJueves.setClickable(false);
+        cbViernes.setClickable(false);
+        cbSabado.setClickable(false);
+        cbDomingo.setClickable(false);
+
+        cbLunes.setChecked(false);
+        cbMartes.setChecked(false);
+        cbMiercoles.setChecked(false);
+        cbJueves.setChecked(false);
+        cbViernes.setChecked(false);
+        cbSabado.setChecked(false);
+        cbDomingo.setChecked(false);
+    }
+
+    public void activarCheck(View view){
+        cbLunes.setClickable(true);
+        cbMartes.setClickable(true);
+        cbMiercoles.setClickable(true);
+        cbJueves.setClickable(true);
+        cbViernes.setClickable(true);
+        cbSabado.setClickable(true);
+        cbDomingo.setClickable(true);
     }
 
 }
